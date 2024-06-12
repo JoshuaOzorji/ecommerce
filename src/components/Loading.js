64bcb15175b7abc780f8b0ee64693a737cf29118
Joshua Ozorji <ozorjijoshua@gmail.com>
2024-06-12 12:40:44 +0100

    fixed auth0 error

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,8 +2,8 @@ import { SpinnerCircularFixed } from "spinners-react";
 
 const Loading = () => {
 	return (
-		<div className="flex justify-center">
-			<SpinnerCircularFixed color="#000000" />
+		<div className='flex justify-center h-full'>
+			<SpinnerCircularFixed color='#000000' />
 		</div>
 	);
 };
