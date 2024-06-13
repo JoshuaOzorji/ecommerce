945255dadffc0e00c73aae817be70b1f248b6779
Joshua Ozorji <ozorjijoshua@gmail.com>
2024-06-13 18:53:27 +0100

    removed setupProxy.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -93,10 +93,10 @@ export const footerLinks = [
 	},
 ];
 
-export const products_url = "/react-store-products";
+// export const products_url = "/react-store-products";
 
-export const single_product_url = `/react-store-single-product?id=`;
+// export const single_product_url = `/react-store-single-product?id=`;
 
-// export const products_url = "https://course-api.com/react-store-products";
+export const products_url = "https://course-api.com/react-store-products";
 
-// export const single_product_url = `https://course-api.com/react-store-single-product?id=`;
+export const single_product_url = `https://course-api.com/react-store-single-product?id=`;
